00f7f20d420ae5eee69a31d841165cc0eccb31dc
Ubuntu <ubuntu@ip-172-31-55-224.ec2.internal>
2022-03-01 14:42:26 +0000

    “fsdkfhnsd

---
+++ hello_world_style.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Created By: Paterry Baptichon Junior
+# Date: Feb. 28, 2022
+# This program displays Hello, World! with proper style.
+
+def main():
+    print("hello, Ladies and Gentlmens ")
+    
+if __name__=="__main__":
+    main()
